2876aa3492184f65ada07e331d9ffba48db1ab1b
Ayman Salah <ayman.1551.salah@gmail.com>
2023-01-14 10:53:51 +0200

    READ'

--- customJquery.js
+++ customJquery.js
@@ -1,3 +1,29 @@
 $(function () {
   console.log("Page is Loaded");
+
+  $("#loadData").on("click", function () {
+    $.ajax({
+      type: "get",
+      url: "http://localhost:3000/Users",
+      success: function (response) {
+        console.log("Done");
+        console.log(response);
+        let allData = response;
+        // allData is an array
+        console.log(allData[0]);
+        $("#tableContent").append(`<tr>
+        <td>1551</td>
+        <td>28</td>
+        <td>Ayman</td>
+        <td>999999</td>
+        <td>
+          *<a href="#">Edit</a>|<a href="#">Delete</a>*
+        </td>
+      </tr>`);
+      },
+    });
+  }); // Loading all JSON data , READ
+
+
+
 }); // End Of Loading Function
