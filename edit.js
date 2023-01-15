e0b40b3c66340b4193f2648bed50ed28d88ff332
Ayman Salah <ayman.1551.salah@gmail.com>
2023-01-15 20:29:45 +0200

    Update Done

--- edit.js
+++ edit.js
@@ -15,20 +15,72 @@ $(function () {
         console.log("The Data");
         console.log(userData);
         console.log(userData.id);
-        $("body").append(`
-        <div class="addRecord adaptive">
-            <input type="text" id="age" placeholder="Age" value="${userData.Age}">
-            <input type="text" id="name" placeholder="Name" value="${userData.Name}">
-            <input type="text" id="salary" placeholder="Salary" value="${userData.Salary}">
-        </div>
-        `);
+        // $("body").append(`
+        // <div class="addRecord adaptive">
+        //     <input type="text" id="age" placeholder="Age" value="${userData.Age}">
+        //     <input type="text" id="name" placeholder="Name" value="${userData.Name}">
+        //     <input type="text" id="salary" placeholder="Salary" value="${userData.Salary}">
+        // </div>
+        // `);
+         $("#age").val(userData.Age);
+        $("#name").val(userData.Name);
+        $("#salary").val(userData.Salary);
       },
     });
   }
 
   getUserData();
-
+  function validateFourFileds() {
+    let ageInput = $("#age").val();
+    let nameInput = $("#name").val();
+    let salaryInput = $("#salary").val();
+    if (
+      isNaN(Number(ageInput)) ||
+      Number(ageInput) < 20 ||
+      Number(ageInput) > 60
+    ) {
+      alert("Error in the Age Field");
+      return 0;
+    } else if (/^[A-Za-z\s]*$/.test(nameInput) != true) {
+      alert("Error in the Name Field");
+      return 0;
+    } else if (isNaN(Number(salaryInput)) || Number(salaryInput) < 0) {
+      alert("Error in the Salary Field");
+      return 0;
+    } else {
+      return 1;
+    }
+  }
   $("#updateData").on("click", function () {
-    console.log("Data Saved") ; 
+    console.log("Data Saved");
+    let result = validateFourFileds();
+    console.log("THE RESULT");
+    console.log(result);
+    if (result === 0) {
+      console.log("Error in the Validation");
+    } else {
+      $.ajax({
+        url: userAPI,
+        type: "put",
+        data: {
+          id: userID ,
+          Age: $("#age").val(),
+          Name: $("#name").val(),
+          Salary: $("#salary").val(),
+        },
+        success: function (res) {
+          alert("Done");
+          $("#myForm").submit() ; 
+          console.log(res);
+        },
+        error: function (Error) {
+         alert("Cant Add Duplicate IDs");
+        }
+      });
+    }
   });
 });
+
+
+
+
